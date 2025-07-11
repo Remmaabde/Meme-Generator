b0989a723bad210fc918aa5fdfcc354492df7632
Reema_abde <Reemabdella702@gmail.com>
2025-07-12 01:13:54 +0300

    Randomly generated images

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -11,16 +11,7 @@ export default function Meme() {
   // Create a state to mantain all memes images
   const [allMemes, setAllMemes] = React.useState([]);
 
-  // As soon as the Meme component loads the first time, make an API call to "https://api.imgflip.com/get_memes"
-  // When the data comes in, save just the memes array part of that data to the `allMemes` state
-
-  // useEffect takes a function as its parameter. If that function
-  // returns something, it needs to be a cleanup function. Otherwise,
-  // it should return nothing. If we make it an async function, it
-  // automatically returns a promise instead of a function or nothing.
-  // Therefore, if you want to use async operations inside of useEffect,
-  // you need to define the function separately inside of the callback
-  // function, as seen below:
+  // 
 
   React.useEffect(() => {
     async function getMemes() {
