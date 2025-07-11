b9d628d5669504e9a29ab54745040d2776871ffb
Reema_abde <Reemabdella702@gmail.com>
2025-07-12 01:17:11 +0300

    Added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import './App.css'
 import Header from '../components/Header'
 import Meme from '../components/Meme'
+import Footer from '../components/Footer'
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
     <>
       <Header/>
       <Meme/>
+      <Footer/>
     </>
   )
 }
